471c47703ac8b03cd001589981ff4074a3acba58
Larry <66905431+larry-oj@users.noreply.github.com>
2023-04-25 14:04:51 +0300

    Projectile direction and destruction

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,29 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private BoxCollider2D _collider;
+    private PlayerMovement _player;
+    private float _speed;
     
     [SerializeField] private float projectileSpeed = 10f;
     
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
+        _player = FindObjectOfType<PlayerMovement>();
+        _speed = _player.transform.localScale.x * projectileSpeed;
+        transform.localScale = new Vector2(Mathf.Sign(_speed), 1f);
     }
     
     private void Update()
     {
-        _rigidbody.velocity = new Vector2(projectileSpeed, 0f);
+        _rigidbody.velocity = new Vector2(_speed, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (_collider.IsTouchingLayers(LayerMask.GetMask("Ground", "Hazard")))
+            Destroy(this.gameObject);
     }
 }
